4071608263d7f2a1578f91032d788a14e587da35
Dusan Jocic <dusanjocic@msn.com>
2015-10-10 16:40:35 +0200

    Fixed problems with loading LUA map hooks

--- game/main/maps/test_lua_helloworld.lua
+++ game/main/maps/test_lua_helloworld.lua
@@ -2,7 +2,7 @@ game.Print("Hello lua world, from ",_VERSION,"!\n")
 
 
 function runCrateFrame(self)
-	//game.Print("runCreateFrame: self is ",self);
+	--game.Print("runCreateFrame: self is ",self); --Dushan: Nobody want massive console spam
 end
 
 function G_InitGame()
@@ -11,12 +11,12 @@ function G_InitGame()
 
 	for i=0,10,1 do
 	  for j=0,10,1 do
-		game.Print("i is ",i, " and j is ",j);
+		--game.Print("i is ",i, " and j is ",j); -- Dushan: Nobody want massive console spam
 		local ent = entity.Spawn("ModelEntity");
 		ent:SetRenderModel("models/props/crates/crate1.obj");
 		ent:SetOrigin(vector.Construct(i*32, j*32, 16));
 		ent:AddEventHandler("runFrame",runCrateFrame);
-		game.Print(ent)
+		--game.Print(ent) -- Dushan: Nobody want massive console spam
 	  end
 	end
 end
